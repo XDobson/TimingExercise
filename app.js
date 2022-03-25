192996e49649f7fdfc92793a74847a30da74fbf2
XDobson <xander.shaffer@stu.caverna.kyschools.us>
2022-03-25 14:07:49 -0500

    timing is key

---
+++ app.js
@@ -0,0 +1,26 @@
+setTimeout(() => {
+    const div1 = document.querySelector(`#first`);
+    const p1 = document.createElement(`p`);
+    p1.innerText = `Hi`;
+    div1.append(p1);
+}, 1000);
+setTimeout(() => {
+    const div2 = document.querySelector(`#timeout-nesting`);
+const p2 = document.createElement(`p`);
+    p2.innerText = `One`;
+    div2.append(p2);
+    setTimeout(() => {
+        const p3 = document.createElement(`p`);
+        p3.innerText = `Two`;
+        div2.append(p3);
+    }, 1000);
+}, 2000);
+let counter = 1;
+const stopwatch = setInterval(() => {
+    console.log(counter);
+    counter++;
+}, 1000);
+const stop = document.querySelector(`button`);
+stop.addEventListener(`click`, () => {
+    clearInterval(stopwatch);
+});
